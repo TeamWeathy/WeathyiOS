ce8e9142f36e65f7e2752ba6119732033f5e448c
seu11ee <yslee2504@naver.com>
2020-12-29 00:01:54 +0900

    [init] xcode project

---
+++ Weathy/Weathy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weathy
+//
+//  Created by 이예슬 on 2020/12/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
