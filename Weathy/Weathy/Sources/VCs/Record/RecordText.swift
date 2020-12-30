0bf8944aca006f0c66f4c986dd9f83309ddfec65
Danna Lee <daeunlee0720@gmail.com>
2020-12-31 07:30:29 +0900

    [add] RecordText 스토리보드, VC 생성

---
+++ Weathy/Weathy/Sources/VCs/Record/RecordText.swift
@@ -0,0 +1,26 @@
+//
+//  RecordText.swift
+//  Weathy
+//
+//  Created by DANNA LEE on 2020/12/31.
+//
+
+import UIKit
+
+class RecordText: UIViewController {
+    
+    //MARK: - IBOutlets
+    
+    
+    
+    
+    //MARK: - LifeCycle Methods
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
