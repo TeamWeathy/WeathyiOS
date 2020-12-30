705313b5a3b1e591ff024a384f40312a7c7ba3c3
Danna Lee <daeunlee0720@gmail.com>
2020-12-31 07:37:24 +0900

    [add] RecordText 모든 컴포넌트, 레이아웃 추가

---
+++ Weathy/Weathy/Sources/VCs/Record/RecordTextVC.swift
@@ -0,0 +1,36 @@
+//
+//  RecordText.swift
+//  Weathy
+//
+//  Created by DANNA LEE on 2020/12/31.
+//
+
+import UIKit
+
+class RecordTextVC: UIViewController {
+    
+    //MARK: - IBOutlets
+    
+    @IBOutlet var backBtn: UIButton!
+    @IBOutlet var dismissBtn: UIButton!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var subTitleLabel: UILabel!
+    @IBOutlet var recordTextField: UITextView!
+    @IBOutlet var nextBtn: UIButton!
+    
+    
+    //MARK: - LifeCycle Methods
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
+
+//MARK: - Style
+
+extension RecordTextVC {
+    
+}
