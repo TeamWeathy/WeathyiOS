7fc043f823f4191edc66a0557f4ee819d76e85c8
Danna Lee <daeunlee0720@gmail.com>
2020-12-31 07:10:05 +0900

    [add] 컬렉션뷰 구현

--- Weathy/Weathy/Sources/Cells/Record/RecordRateCVC.swift
+++ Weathy/Weathy/Sources/Cells/Record/RecordRateCVC.swift
@@ -15,4 +15,16 @@ class RecordRateCVC: UICollectionViewCell {
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var descriptionLabel: UILabel!
     
+    func setCell(imageName: String, titleText: String, titleColor: String, description: String) {
+        emojiImageView.image = UIImage(named: "\(imageName)")
+        
+        titleLabel.text = "\(titleText)"
+//        titleLabel.textColor = UIColor.
+        titleLabel.font = UIFont(name: "AppleSDGothicNeoSB00", size: 17)
+        
+        descriptionLabel.text = "\(description)"
+        descriptionLabel.font = UIFont(name: "AppleSDGothicNeoR00", size: 16)
+        descriptionLabel.textColor = UIColor.lightGray
+    }
+    
 }
--- Weathy/Weathy/Sources/VCs/Record/RecordRateVC.swift
+++ Weathy/Weathy/Sources/VCs/Record/RecordRateVC.swift
@@ -11,6 +11,16 @@ class RecordRateVC: UIViewController {
     
     //MARK: - Custom Variables
     
+    struct Rates {
+        let emoji: String
+        let title: String
+        let titleColor: String
+        let explanation: String
+        var isSelected: Bool
+    }
+    
+    var rate: [Rates] = []
+    
     
     //MARK: - IBOutlets
     
@@ -31,6 +41,10 @@ class RecordRateVC: UIViewController {
         
         setAboveCollection()
         setNextBtn()
+        setCV()
+        
+        rateCollectionView.dataSource = self
+        rateCollectionView.delegate = self
 
         // Do any additional setup after loading the view.
     }
@@ -55,10 +69,96 @@ extension RecordRateVC {
 //        stepTwoImageView.image = UIImage(named: <#T##String#>)
     }
     
+    func setCV() {
+        rate = [
+            Rates(emoji: "pencil.circle", title: "너무 더워요", titleColor: "black", explanation: "훨씬 얇게 입을걸 그랬어요.", isSelected: false),
+            Rates(emoji: "pencil.circle", title: "더워요", titleColor: "black", explanation: "좀 더 가볍게 입을걸 그랬어요.", isSelected: false),
+            Rates(emoji: "pencil.circle", title: "적당해요", titleColor: "black", explanation: "딱 적당하게 입었어요.", isSelected: false),
+            Rates(emoji: "pencil.circle", title: "추워요", titleColor: "black", explanation: "좀 더 따뜻하게 입을걸 그랬어요.", isSelected: false),
+            Rates(emoji: "pencil.circle", title: "너무 추워요", titleColor: "black", explanation: "훨씬 두껍게 입을걸 그랬어요.", isSelected: false)
+        ]
+    }
+    
     func setNextBtn() {
         nextBtn.backgroundColor = UIColor.lightGray
         nextBtn.setTitle("다음", for: .normal)
         nextBtn.setTitleColor(.white, for: .normal)
         nextBtn.layer.cornerRadius = 30
     }
+}
+
+//MARK: - UICollectionViewDataSource
+
+extension RecordRateVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        rate.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RecordRateCVC", for: indexPath) as? RecordRateCVC
+        else {
+            return UICollectionViewCell()
+        }
+        
+        cell.setCell(imageName: rate[indexPath.item].emoji, titleText: rate[indexPath.item].title, titleColor: rate[indexPath.item].titleColor, description: rate[indexPath.item].explanation)
+        
+        cell.layer.borderWidth = 1
+        cell.layer.cornerRadius = 20
+        
+        /// 분기처리
+        if rate[indexPath.item].isSelected == true {
+            cell.layer.borderColor = UIColor(red: 0.506, green: 0.886, blue: 0.824, alpha: 1).cgColor
+            cell.layer.shadowRadius = 20
+            cell.layer.shadowColor = UIColor(red: 0.506, green: 0.886, blue: 0.824, alpha: 0.4).cgColor
+            cell.layer.shadowOffset = CGSize(width: 0, height: 0)
+            cell.layer.masksToBounds = false
+            cell.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner, .layerMinXMinYCorner, .layerMaxXMinYCorner]
+        } else {
+            cell.layer.borderColor = UIColor.lightGray.cgColor
+            cell.layer.shadowRadius = 0
+        }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        collectionView.deselectItem(at: indexPath, animated: false)
+        
+        /// 선택된 거 빼고 모두 isSelected를 false로 변경
+        for i in 0...rate.count-1 {
+            if i == indexPath.item {
+                rate[i].isSelected = true
+            } else {
+                rate[i].isSelected = false
+            }
+        }
+        
+        self.rateCollectionView.reloadData()
+    }
+}
+
+
+//MARK: - UICollectionViewDelegate
+
+extension RecordRateVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            
+            let cellWidth : CGFloat = collectionView.frame.width
+            let cellHeight : CGFloat = 80
+            
+            return CGSize(width: cellWidth, height: cellHeight)
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+            return 12
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+            return 0
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+            return UIEdgeInsets(top: 0, left: 0, bottom: 1, right: 0)
+        }
 }
