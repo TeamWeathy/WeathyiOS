77c6eb6223c17c4c4cfb28275bf3c0c2517956b5
Danna Lee <daeunlee0720@gmail.com>
2020-12-31 05:39:23 +0900

    [add] RecordRate 스토리보드, VC 생성

---
+++ Weathy/Weathy/Sources/VCs/Record/RecordRateVC.swift
@@ -0,0 +1,19 @@
+//
+//  RecordRateVC.swift
+//  Weathy
+//
+//  Created by DANNA LEE on 2020/12/31.
+//
+
+import UIKit
+
+class RecordRateVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
