0f52fccb3c275d58cccfb389a80248bd0f42edc7
Danna Lee <daeunlee0720@gmail.com>
2021-01-01 19:48:00 +0900

    [add] UIFont extension 추가

---
+++ Weathy/Weathy/Sources/Extensions/UIFont+Extension.swift
@@ -0,0 +1,93 @@
+//
+//  UIFont+Extension.swift
+//  Weathy
+//
+//  Created by DANNA LEE on 2021/01/01.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    /// AppleSDGothicNeo Regular
+    class var SDGothicRegular13: UIFont{
+        return UIFont(name: "AppleSDGothicNeoR00", size: 13)!
+    }
+    
+    class var SDGothicRegular15: UIFont{
+        return UIFont(name: "AppleSDGothicNeoR00", size: 15)!
+    }
+    
+    class var SDGothicRegular16: UIFont{
+        return UIFont(name: "AppleSDGothicNeoR00", size: 16)!
+    }
+    
+    class var SDGothicRegular18: UIFont{
+        return UIFont(name: "AppleSDGothicNeoR00", size: 18)!
+    }
+    
+    class var SDGothicRegular20: UIFont{
+        return UIFont(name: "AppleSDGothicNeoR00", size: 20)!
+    }
+    
+    
+    /// ApleSDGothicNeo Medium
+    class var SDGothicMedium15: UIFont{
+        return UIFont(name: "AppleSDGothicNeoM00", size: 15)!
+    }
+    
+    class var SDGothicMedium16: UIFont{
+        return UIFont(name: "AppleSDGothicNeoM00", size: 16)!
+    }
+    
+    
+    /// AppleSDGothicNeo Semibold
+    class var SDGothicSemiBold13: UIFont{
+        return UIFont(name: "AppleSDGothicNeoSB00", size: 13)!
+    }
+    
+    class var SDGothicSemiBold16: UIFont{
+        return UIFont(name: "AppleSDGothicNeoSB00", size: 16)!
+    }
+    
+    class var SDGothicSemiBold17: UIFont{
+        return UIFont(name: "AppleSDGothicNeoSB00", size: 17)!
+    }
+    
+    class var SDGothicSemiBold18: UIFont{
+        return UIFont(name: "AppleSDGothicNeoSB00", size: 18)!
+    }
+    
+    class var SDGothicSemiBold20: UIFont{
+        return UIFont(name: "AppleSDGothicNeoSB00", size: 20)!
+    }
+    
+    class var SDGothicSemiBold23: UIFont{
+        return UIFont(name: "AppleSDGothicNeoSB00", size: 23)!
+    }
+    
+    class var SDGothicSemiBold25: UIFont{
+        return UIFont(name: "AppleSDGothicNeoSB00", size: 25)!
+    }
+    
+    
+    /// Roboto Regular
+    class var RobotoRegular15: UIFont{
+        return UIFont(name: "Roboto-Regular", size: 15)!
+    }
+    
+    class var RobotoRegular18: UIFont{
+        return UIFont(name: "Roboto-Regular", size: 18)!
+    }
+    
+    class var RobotoRegular25: UIFont{
+        return UIFont(name: "Roboto-Regular", size: 25)!
+    }
+    
+    class var RobotoRegular60: UIFont{
+        return UIFont(name: "Roboto-Regular", size: 60)!
+    }
+    
+    
+    
+}
