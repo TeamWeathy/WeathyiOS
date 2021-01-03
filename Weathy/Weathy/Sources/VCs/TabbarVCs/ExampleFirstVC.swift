a3dac975846ff00d1652b0501400f9f01b831e2f
Hwangho <63707317+Hwangho@users.noreply.github.com>
2021-01-04 02:54:09 +0900

    [add] Tabbar gradient 성공

---
+++ Weathy/Weathy/Sources/VCs/TabbarVCs/ExampleFirstVC.swift
@@ -0,0 +1,57 @@
+//
+//  ExampleFirstVC.swift
+//  Weathy
+//
+//  Created by 송황호 on 2021/01/03.
+//
+
+import UIKit
+
+class ExampleFirstVC: UIViewController {
+
+    static let identifier = "ExampleFirstVC"
+    
+    var gradientLayer: CAGradientLayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setGradientView()
+    }
+
+    /// 뒤 배경에 그라데이션을 넣기 위한 방법
+    func setGradientView(){
+       
+        /// View 생성
+        let gradientView = UIView()
+        self.view.addSubview(gradientView)
+        
+        /// View에 대한 size를 정의 해줘야만 가능!!
+        gradientView.frame.size = .init(width: self.view.frame.width, height: 54)
+        gradientView.backgroundColor = UIColor.clear
+        
+        /// View에 그라데이션 추가
+        self.gradientLayer = CAGradientLayer()
+        self.gradientLayer.frame = gradientView.bounds
+        self.gradientLayer.colors = [UIColor.white.withAlphaComponent(0).cgColor, UIColor.white.withAlphaComponent(0.3).cgColor, UIColor.white.cgColor]
+        gradientView.layer.addSublayer(self.gradientLayer)
+        
+        /// grandientView 레이아웃 잡기
+        gradientView.translatesAutoresizingMaskIntoConstraints = false
+        gradientView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        gradientView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        gradientView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        /// grandientView size 설정
+        gradientView.heightAnchor.constraint(equalToConstant: 54).isActive = true
+        gradientView.widthAnchor.constraint(equalToConstant: self.view.frame.width).isActive = true
+        
+        /// gradient 위에서 아래로 주기
+        self.gradientLayer.locations = [0.5, 0.7]
+        
+        ///gradient를 가로로 만들어주는 시작점과 끝점 포인트
+//        self.gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
+//        self.gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
+    }
+}
+
+
---
+++ Weathy/Weathy/Sources/VCs/TabbarVCs/ExampleThirdVC.swift
@@ -0,0 +1,31 @@
+//
+//  ExampleThirdVC.swift
+//  Weathy
+//
+//  Created by 송황호 on 2021/01/03.
+//
+
+import UIKit
+
+class ExampleThirdVC: UIViewController {
+
+    static let identifier = "ExampleThirdVC"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
