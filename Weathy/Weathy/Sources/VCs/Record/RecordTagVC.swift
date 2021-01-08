29325527828163b43a8e54e6d5f2ba21d74b9ad4
Danna Lee <daeunlee0720@gmail.com>
2021-01-09 04:50:28 +0900

    [add] 토스트 구현

--- Weathy/Weathy/Sources/VCs/Record/RecordTagVC.swift
+++ Weathy/Weathy/Sources/VCs/Record/RecordTagVC.swift
@@ -381,6 +381,29 @@ extension RecordTagVC {
             self.explanationLabel.frame = CGRect(x: self.explanationLabel.frame.origin.x, y: self.explanationLabel.frame.origin.y+10, width: self.explanationLabel.frame.width, height: self.explanationLabel.frame.height)
         })
     }
+    
+    func showToast(message: String) {
+        let width_variable: CGFloat = 33
+        let toastLabel = UILabel(frame: CGRect(x: width_variable, y: self.view.frame.size.height-165, width: view.frame.size.width-2*width_variable, height: 50))
+        // 뷰가 위치할 위치를 지정해준다. 여기서는 아래로부터 100만큼 떨어져있고, 너비는 양쪽에 10만큼 여백을 가지며, 높이는 35로
+        toastLabel.backgroundColor = UIColor(red: 202/255, green: 241/255, blue: 235/255, alpha: 0.6)
+        toastLabel.textColor = UIColor.mainGrey
+        toastLabel.textAlignment = .center;
+        toastLabel.font = UIFont.SDGothicRegular16
+        toastLabel.lineSetting(kernValue: -0.8)
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 29.5
+        toastLabel.clipsToBounds  =  true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 2.0, delay: 1.0, options: .curveEaseOut, animations: {
+            toastLabel.alpha = 0.0
+        }, completion: { (isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
+    }
+
+
 }
 
 //MARK: - UICollectionViewDataSource
@@ -491,6 +514,9 @@ extension RecordTagVC: UICollectionViewDataSource {
                     if tagTitles[titleIndex].tagTab[indexPath.item].isSelected == true {
                         tagTitles[titleIndex].tagTab[indexPath.item].isSelected = false
                         tagTitles[titleIndex].count -= 1
+                    }
+                    else {
+                        showToast(message: "태그는 카테고리당 5개만 선택할 수 있어요.")
                     }
                 }
                 else {
