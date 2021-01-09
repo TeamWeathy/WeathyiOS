c52c587e1e26dc7f038f29687942e1c10a1c8a54
inaeLee <inae9709@hanmail.net>
2021-01-09 18:56:54 +0900

    [add] todayWeathyView 레이아웃 및 폰트, 색상 적용

--- Weathy/Weathy/Sources/Cells/Main/MainTopCVC.swift
+++ Weathy/Weathy/Sources/Cells/Main/MainTopCVC.swift
@@ -18,6 +18,18 @@ class MainTopCVC: UICollectionViewCell {
     @IBOutlet weak var lowTemperatureLabel: UILabel!
     @IBOutlet weak var climateLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var weathyDateLabel: UILabel!
+    @IBOutlet weak var weathyClimateImage: UIImageView!
+    @IBOutlet weak var weathyClimateLabel: UILabel!
+    @IBOutlet weak var weathyHighTemperatureLabel: UILabel!
+    @IBOutlet weak var weathyLowTemperatureLabel: UILabel!
+    @IBOutlet weak var weathyStampImage: UIImageView!
+    @IBOutlet weak var weathyStampLabel: UILabel!
+    @IBOutlet weak var closetTopLabel: UILabel!
+    @IBOutlet weak var closetBottomLabel: UILabel!
+    @IBOutlet weak var closetOuterLabel: UILabel!
+    @IBOutlet weak var closetEtcLabel: UILabel!
+    
     
     //MARK: - Custom Methods
     func setCell() {
@@ -31,20 +43,41 @@ class MainTopCVC: UICollectionViewCell {
         locationLabel.font = UIFont.SDGothicSemiBold20
         locationLabel.textColor = UIColor.mainGrey
         
-        currTemperatureLabel.text = "2°"
+        currTemperatureLabel.text = "-2°"
         currTemperatureLabel.font = UIFont.RobotoLight60
         currTemperatureLabel.textColor = UIColor.subGrey1
         
-        highTemperatureLabel.text = "4°"
+        highTemperatureLabel.text = "24°"
         highTemperatureLabel.font = UIFont.RobotoLight25
         highTemperatureLabel.textColor = UIColor.redTemp
         
-        lowTemperatureLabel.text = "-4°"
+        lowTemperatureLabel.text = "-14°"
         lowTemperatureLabel.font = UIFont.RobotoLight25
         lowTemperatureLabel.textColor = UIColor.blueTemp
         
         climateLabel.text = "조금 흐리지만\n햇살이 따스해요 :)"
         climateLabel.font = UIFont.SDGothicRegular16
         climateLabel.textColor = UIColor.init(red: 0, green: 0, blue: 0, alpha: 0.58)
+        
+        weathyDateLabel.text = "2020년 12월 1일"
+        weathyDateLabel.font = UIFont.SDGothicRegular13
+        weathyDateLabel.textColor = UIColor.subGrey6
+        
+        weathyClimateLabel.text = "구름 조금"
+        weathyClimateLabel.font = UIFont.SDGothicRegular15
+        weathyClimateLabel.textColor = UIColor.subGrey1
+        
+        weathyHighTemperatureLabel.text = "4°"
+        weathyHighTemperatureLabel.textColor = UIColor.redTemp
+        weathyHighTemperatureLabel.font = UIFont.RobotoLight30
+
+        weathyLowTemperatureLabel.text = "-4°"
+        weathyLowTemperatureLabel.textColor = UIColor.blueTemp
+        weathyLowTemperatureLabel.font = UIFont.RobotoLight30
+        
+//        weathyStampImage.image = UIImage(named: <#T##String#>)
+        weathyStampLabel.text = "추워요"
+        weathyStampLabel.font = UIFont.SDGothicSemiBold23
+        weathyStampLabel.textColor = UIColor.imojiColdText
     }
 }
