b278fa9c69dd40db1bd33990fa44012383d26d74
Danna Lee <daeunlee0720@gmail.com>
2021-01-13 06:45:21 +0900

    [add] 뷰컨, 스토리보드 생성

---
+++ Weathy/Weathy/Sources/VCs/Splash/SplashVC.swift
@@ -0,0 +1,18 @@
+//
+//  SplashVC.swift
+//  Weathy
+//
+//  Created by DANNA LEE on 2021/01/13.
+//
+
+import UIKit
+
+class SplashVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
