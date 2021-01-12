840d870fbd5dfd35e0e471b5d4bdbd847ee09716
Danna Lee <daeunlee0720@gmail.com>
2021-01-13 07:07:39 +0900

    [add] 스플래시 구현
    근데 왜 까만색만 보일까 ...

--- Weathy/Weathy/Sources/VCs/Splash/SplashVC.swift
+++ Weathy/Weathy/Sources/VCs/Splash/SplashVC.swift
@@ -6,13 +6,29 @@
 //
 
 import UIKit
+import Lottie
 
 class SplashVC: UIViewController {
+    
+    let animationView = AnimationView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        displaySplash()
+    }
+    
+    func displaySplash() {
+        animationView.animation = Animation.named("스플래시.json")
+        animationView.frame = view.bounds
+        animationView.contentMode = .scaleAspectFill
+        animationView.loopMode = .loop
+        animationView.play()
+        view.addSubview(animationView)
     }
 
 }
