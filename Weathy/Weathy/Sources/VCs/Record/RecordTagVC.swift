5aa568cefb1112c69ee6c92b3001fce44f7d4f3a
Danna Lee <daeunlee0720@gmail.com>
2021-01-14 06:44:43 +0900

    [fix] showToast extension으로 변경
    self.showToast

--- Weathy/Weathy/Sources/VCs/Record/RecordTagVC.swift
+++ Weathy/Weathy/Sources/VCs/Record/RecordTagVC.swift
@@ -119,13 +119,13 @@ class RecordTagVC: UIViewController {
     @objc func tagAdded(_ noti : Notification){
         let isAdded = noti.object
         self.viewWillAppear(true)
-        showToast(message: "태그가 추가되었어요!")
+        self.showToast(message: "태그가 추가되었어요!")
     }
     
     @objc func tagDeleted(_ noti : Notification){
         let isDeleted = noti.object
         print(">>> received")
-        showToast(message: "태그가 삭제되었어요!")
+        self.showToast(message: "태그가 삭제되었어요!")
     }
     
     @objc func longTap(gesture : UILongPressGestureRecognizer!) {
@@ -298,27 +298,6 @@ extension RecordTagVC {
         UIView.animate(withDuration: 1, delay: 0.5, animations: {
             self.explanationLabel.alpha = 1
             self.explanationLabel.frame = CGRect(x: self.explanationLabel.frame.origin.x, y: self.explanationLabel.frame.origin.y+10, width: self.explanationLabel.frame.width, height: self.explanationLabel.frame.height)
-        })
-    }
-    
-    func showToast(message: String) {
-        let width_variable: CGFloat = 33
-        let toastLabel = UILabel(frame: CGRect(x: width_variable, y: self.view.frame.size.height-165, width: view.frame.size.width-2*width_variable, height: 50))
-        // 뷰가 위치할 위치를 지정해준다. 여기서는 아래로부터 100만큼 떨어져있고, 너비는 양쪽에 10만큼 여백을 가지며, 높이는 35로
-        toastLabel.backgroundColor = UIColor(red: 202/255, green: 241/255, blue: 235/255, alpha: 0.6)
-        toastLabel.textColor = UIColor.mainGrey
-        toastLabel.textAlignment = .center;
-        toastLabel.font = UIFont.SDGothicRegular16
-        toastLabel.lineSetting(kernValue: -0.8)
-        toastLabel.text = message
-        toastLabel.alpha = 1.0
-        toastLabel.layer.cornerRadius = 29.5
-        toastLabel.clipsToBounds  =  true
-        self.view.addSubview(toastLabel)
-        UIView.animate(withDuration: 2.0, delay: 1.0, options: .curveEaseOut, animations: {
-            toastLabel.alpha = 0.0
-        }, completion: { (isCompleted) in
-            toastLabel.removeFromSuperview()
         })
     }
     
