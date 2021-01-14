80ede60c3a1625f925b0f32a7e2ff8c305ecf5b2
Danna Lee <daeunlee0720@gmail.com>
2021-01-15 06:05:03 +0900

    [fix] 테스트 날짜 수정

--- Weathy/Weathy/Sources/VCs/ModifyWeathy/ModifyWeathyStartVC.swift
+++ Weathy/Weathy/Sources/VCs/ModifyWeathy/ModifyWeathyStartVC.swift
@@ -14,7 +14,7 @@ class ModifyWeathyStartVC: UIViewController {
     var todayMonth: Int = 1
     var todayDate: Int = 1
     
-    var fullDate: String = "2021-01-25"
+    var fullDate: String = "2021-01-13"
     var month: Int = 12
     var date: Int = 20
     var day: String = "월"
