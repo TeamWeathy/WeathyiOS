35bbf9888768a1405bf082b74951dc678272b2e5
seu11ee <yslee2504@naver.com>
2021-01-16 07:08:21 +0900

    [update] 토스트 extension 수정

--- Weathy/Weathy/Sources/Extensions/UIViewController+Extension.swift
+++ Weathy/Weathy/Sources/Extensions/UIViewController+Extension.swift
@@ -10,7 +10,7 @@ import UIKit
 extension UIViewController{
     func showToast(message: String) {
         let width_variable: CGFloat = 33
-        let toastLabel = UILabel(frame: CGRect(x: width_variable, y: self.view.frame.size.height-165, width: view.frame.size.width-2*width_variable, height: 50))
+        let toastLabel = UILabel(frame: CGRect(x: width_variable, y: UIScreen.main.bounds.height-165, width: view.frame.size.width-2*width_variable, height: 50))
         // 뷰가 위치할 위치를 지정해준다. 여기서는 아래로부터 100만큼 떨어져있고, 너비는 양쪽에 10만큼 여백을 가지며, 높이는 35로
         toastLabel.backgroundColor = UIColor(red: 202/255, green: 241/255, blue: 235/255, alpha: 0.6)
         toastLabel.textColor = UIColor.mainGrey
