7529a3204d47398022cb12855c8de4d4e8cb068c
inaeLee <inae9709@hanmail.net>
2021-02-12 21:13:22 +0900

    add: scrollView version 뷰 전체 틀 잡기

---
+++ Weathy/Weathy/Sources/VCs/Main/MainTempVC.swift
@@ -0,0 +1,29 @@
+//
+//  MainTempVC.swift
+//  Weathy
+//
+//  Created by inae Lee on 2021/02/12.
+//
+
+import UIKit
+
+class MainTempVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
