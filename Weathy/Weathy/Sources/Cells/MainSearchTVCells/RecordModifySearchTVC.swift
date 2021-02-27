28a44f9fb873fa46cf6c71b6762a36f54db523e8
Danna Lee <daeunlee0720@gmail.com>
2021-02-28 08:22:53 +0900

    [fix] 과거 데이터 검색하기 디자인 반영

--- Weathy/Weathy/Sources/Cells/MainSearchTVCells/RecordModifySearchTVC.swift
+++ Weathy/Weathy/Sources/Cells/MainSearchTVCells/RecordModifySearchTVC.swift
@@ -16,11 +16,11 @@ class RecordModifySearchTVC: UITableViewCell {
     //MARK: - IBOutlets
     
     @IBOutlet weak var radiusView: UIView!
-    @IBOutlet weak var weatherDateLabel: UILabel!
-    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var weatherDateLabel: SpacedLabel!
+    @IBOutlet weak var locationLabel: SpacedLabel!
     @IBOutlet weak var weatherImageView: UIImageView!
-    @IBOutlet weak var maxTempLabel: UILabel!
-    @IBOutlet weak var minTempLabel: UILabel!
+    @IBOutlet weak var maxTempLabel: SpacedLabel!
+    @IBOutlet weak var minTempLabel: SpacedLabel!
     @IBOutlet weak var slashLabel: UILabel!
     
     //MARK: - LifeCycle Methods
@@ -29,7 +29,7 @@ class RecordModifySearchTVC: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
         
-        shadowSet()
+        setRadiusView()
         textColor()
         textFont()
         setSlashLabel()
@@ -53,22 +53,27 @@ extension RecordModifySearchTVC {
         self.minTempLabel.text = "\(lowTemper)"
     }
     
-    func shadowSet(){
+    func setRadiusView(){
         radiusView.layer.cornerRadius = 35
-        radiusView.backgroundColor  = .white
+        radiusView.backgroundColor  = UIColor.white.withAlphaComponent(0.75)
         radiusView.setBorder(borderColor: .subGrey7, borderWidth: 1)
+        
     }
     
     func textFont(){
         weatherDateLabel.font = UIFont.SDGothicRegular15
-        weatherDateLabel.textColor = UIColor.subGrey1
+        weatherDateLabel.characterSpacing = -0.75
         locationLabel.font = UIFont.SDGothicSemiBold17
-        locationLabel.textColor = UIColor.subGrey1
+        locationLabel.characterSpacing = -0.85
         maxTempLabel.font = UIFont(name: "Roboto-Light", size: 40)
+        maxTempLabel.characterSpacing = -2.0
         minTempLabel.font = UIFont(name: "Roboto-Light", size: 40)
+        maxTempLabel.characterSpacing = -2.0
     }
     
     func textColor(){
+        locationLabel.textColor = UIColor.subGrey1
+        weatherDateLabel.textColor = .subGrey1
         maxTempLabel.textColor = .redTemp
         minTempLabel.textColor = .blueTemp
     }
